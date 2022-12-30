3507a3b57cb006c9aca86f5f1db5fdfb07aa0bc9
Luis Grados Salinas <martin.programmer@gmail.com>
2022-12-30 13:48:45 -0500

    Update stg_posts_answers.sql

--- models/staging/stg_posts_answers.sql
+++ models/staging/stg_posts_answers.sql
@@ -6,5 +6,5 @@ select
     body,
     owner_user_id,
     cast(parent_id as string) as parent_id
-from {{ source("stackoverflow", "posts_answers") }}
+from {{ source("raw", "posts_answers") }}
 where creation_date >= timestamp("2022-12-01")
