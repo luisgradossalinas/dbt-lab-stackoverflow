ae369b15b775fd96371cf16c2b397b8dfdb4abe8
Luis Grados Salinas <martin.programmer@gmail.com>
2023-01-11 19:17:02 -0500

    Update posts_combined.sql

--- models/marts/posts_combined.sql
+++ models/marts/posts_combined.sql
@@ -1,7 +1,21 @@
-select post_id, created_at, type, title, body, owner_user_id, parent_id
+select 
+    post_id, 
+    created_at, 
+    type, 
+    title, 
+    body, 
+    owner_user_id, 
+    parent_id
 from {{ ref("stg_posts_answers") }}
 
 union all
 
-select post_id, created_at, type, title, body, owner_user_id, parent_id
+select 
+    post_id, 
+    created_at, 
+    type, 
+    title, 
+    body, 
+    owner_user_id, 
+    parent_id
 from {{ ref("stg_posts_questions") }}
